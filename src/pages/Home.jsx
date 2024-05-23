95d13fac878532ac0190c721c94dcb608eec971a
NorScorpio91 <notscorpio91@gmail.com>
2024-05-23 15:51:23 +0500

    user added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Card from '../components/CardHover'
 
 function Home() {
   return (
     <div className="h-screen w-screen bg-slate-800 text-white flex flex-col justify-center items-center">
-   
+    <Card />
     </div>
   )
 }
