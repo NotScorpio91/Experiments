d3656a81e81df8b172ee4a71356d48066550a088
KING 69 <kingbye69@gmail.com>
2024-05-23 15:47:01 +0500

    route added

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+    Home,
+} from './pages/Index'
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="" element={<Home />} />
+                    {/* <Route path="/quote" element={<Quote />} />
+                    <Route path="/hire-dev" element={<HireDev />} />
+                    <Route path="/project" element={<Project />} />
+                    <Route path="/work/:slug" element={<ServicesPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/career" element={<Career />} />
+                    <Route path="/career/:slug" element={<CareerForm/>} />
+                    <Route path="*" exact element={<PageNotfound />} /> */}
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import React from "react"
-import Card from "./components/CardHover"
+import React from 'react'
 
-function App() {
-
+function Home() {
   return (
-    <>
     <div className="h-screen w-screen bg-slate-800 text-white flex flex-col justify-center items-center">
-      <Card />
+   
     </div>
-    </>
   )
 }
 
-export default App
+export default Home
